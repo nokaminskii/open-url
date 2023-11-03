f392683d9b8dd635b0c2416b0e28e8c9abb76c05
Nik <nokaminskii@gmail.com>
2023-11-03 10:14:25 +0300

    user_login_current_url

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 def test_user_login():
-    user_name = "nokaminskii@gmail.com"
-    password = "123456"
-    browser_url ="https://magento.softwaretestingboard.com/customer/account/login"
+    user_name = "minskw434i@gmail.com"
+    password = "dsewerer"
+    browser_url ="https://portal.ustraveldocs.com/"
 
     driver = webdriver.Chrome()
     driver.implicitly_wait(5)
+    time.sleep(5)
     driver.get(browser_url)
     driver.maximize_window()
-    driver.find_element(By.ID, "email").send_keys(user_name)
-    driver.find_element(By.ID, "pass").send_keys(password)
-    driver.find_element(By.ID, "send2").click()
-    driver.implicitly_wait(5)
-    error_message = driver.find_element(By.CSS_SELECTOR, '[data-ui-id="message-error"]').text
-    assert (error_message == "The account sign-in was incorrect or your account is disabled temporarily."
-                             " Please wait and try again later.")
+    driver.find_element(By.ID, "loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:username").send_keys(user_name)
+    driver.implicitly_wait(15)
+    time.sleep(5)
+    driver.find_element(By.ID, "loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:password").send_keys(password)
+    driver.implicitly_wait(15)
+    time.sleep(5)
+    driver.find_element(By.NAME, "loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:j_id167").click()
+    driver.implicitly_wait(15)
+    time.sleep(5)
+    driver.find_element(By.ID, "loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:loginButton").click()
+    driver.implicitly_wait(15)
+    time.sleep(10)
+    error_message = driver.find_element(By.ID, "loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:error:j_id132:j_id133:0:j_id134:j_id135:j_id137").text
+    assert (error_message == "Ошибка.Неудачная попытка входа. Введите правильное имя пользователя и пароль.")
 
 
-
+if __name__ == "__main__":
+    test_user_login()
